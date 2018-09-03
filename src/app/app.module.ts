bcfb582681ca6881f1e0fc4e9dd6443b89b9cc92
Joel <Joel@joel.gonzaga>
2018-09-03 14:28:50 -0700

    Filler Text and images added. CSS modified for fonts and formatting.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
