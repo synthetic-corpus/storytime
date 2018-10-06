31a6101e32be26aa4ec31a6d9ff5f4c92f4ef44f
Joel <Joel@joel.gonzaga>
2018-10-06 15:49:33 -0700

    Added Header and footer. Stylized.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BodyComponent } from './body/body.component';
 
 import { environment } from '../environments/environment';
 import { OneComponent } from './chapters/1-one/one.component';
@@ -14,11 +13,12 @@ import { SixComponent } from './chapters/6-six/six.component';
 import { SevenComponent } from './chapters/7-seven/seven.component';
 import { EightComponent } from './chapters/8-eight/eight.component';
 import { NineComponent } from './chapters/9-nine/nine.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BodyComponent,
     OneComponent,
     TwoComponent,
     ThreeComponent,
@@ -27,7 +27,9 @@ import { NineComponent } from './chapters/9-nine/nine.component';
     SixComponent,
     SevenComponent,
     EightComponent,
-    NineComponent
+    NineComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
